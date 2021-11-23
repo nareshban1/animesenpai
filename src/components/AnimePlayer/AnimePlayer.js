1d3a32c68a03e07fb048b6405804fd973225c459
Naresh Ban <np03a170060@heraldcollege.edu.np>
2021-11-23 15:15:34 +0545

    test

--- src/components/AnimePlayer/AnimePlayer.js
+++ src/components/AnimePlayer/AnimePlayer.js
@@ -6,7 +6,7 @@ function AnimePlayer({ currentEpisode }) {
       <VideoContainer>
         <VideoPlayer
           controls={true}
-          url={"https://api.aniapi.com/v1/proxy/https%3a%2f%2fgogoplay1.com%2fstreaming.php%3fid%3dMzUxOA%3d%3d%26title%3dOne%2bPiece%2bEpisode%2b1/gogoanime/"}
+          url={currentEpisode?.video}
           config={{
             file: {
               forceHLS: true,
